544a70f49e3c3936093e1fa6614976cc7f264332
Sarthik <sarthikbangroo@gmail.com>
2024-09-27 00:06:09 +0530

    Implement view available books functionality

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,15 @@ function showMenu() {
     console.log('4. View Available Books');
     console.log('5. Exit');
 }
+
+function addBook() {
+    const isbn = readline.question('Enter ISBN: ');
+    const title = readline.question('Enter Title: ');
+    const author = readline.question('Enter Author: ');
+    const year = parseInt(readline.question('Enter Publication Year: '), 10);
+
+    const book = new Book(isbn, title, author, year);
+    library.addBook(book);
+    console.log('Book added successfully.');
+}
+
